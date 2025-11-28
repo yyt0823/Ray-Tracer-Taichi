61ecaff5ea21854a0bb9f729ff8918aed6a4d391
YYT <yantianyin1999@gmail.com>
2025-11-28 10:04:13 -0500

    obj1 done

--- camera.py
+++ camera.py
@@ -3,6 +3,7 @@ from helperclasses import Ray
 import taichi as ti
 from pyglm import glm
 import taichi.math as tm
+import math
 
 @ti.data_oriented
 class Camera:
@@ -28,17 +29,34 @@ class Camera:
 
 		# TODO: Objective 1: Compute camera frame basis vectors, and top bottom left right for ray generation
 		# NOTE: glm vectors are passed in to permit the work to be done here in python, but stored vectors must be tm.vec3
-
-
-		# Compute proper values below!
-		self.top = 1
-		self.bottom = -1
-		self.left = -1
-		self.right = 1
-		self.u = tm.vec3(1,0,0)
-		self.v = tm.vec3(0,0,0)
-		self.w = tm.vec3(0,0,1)
 		self.eye_position = tm.vec3(eye_position.x, eye_position.y, eye_position.z)
+
+		# set up for u v w 
+		w_glm = glm.normalize(eye_position - lookat)
+		self.w = tm.vec3(w_glm.x, w_glm.y, w_glm.z)
+
+		u_glm = glm.normalize(glm.cross(up, w_glm))
+		self.u = tm.vec3(u_glm.x, u_glm.y, u_glm.z)
+
+		v_glm = glm.cross(w_glm, u_glm)
+		self.v = tm.vec3(v_glm.x, v_glm.y, v_glm.z)
+
+		# set up for top bottom left right
+		fovy_rad = math.radians(fovy)
+		half_height = self.distance_to_plane * math.tan(fovy_rad / 2.0)
+
+		self.top = half_height
+		self.bottom = -half_height
+
+		aspect_ratio = self.width / self.height
+		half_width = half_height * aspect_ratio
+
+		self.right = half_width
+		self.left = -half_width
+		
+		
+	
+
 
 
 
@@ -58,6 +76,19 @@ class Camera:
 
 		
 		# TODO: Objective 1: Generate ray from camera through pixel (col, row) with jittering
+		x_offset = 0.5
+		y_offset = 0.5
+		if jitter: 
+			# Sub-pixel offset should be random between 0.0 and 1.0
+			x_offset = ti.random(float)
+			y_offset = ti.random(float)
+		u_coord = self.left + (x + x_offset) * (self.right - self.left) / self.width
+		v_coord = self.bottom + (y + y_offset) * (self.top - self.bottom) / self.height
+		d = (
+            - self.distance_to_plane * self.w
+            + u_coord * self.u
+            + v_coord * self.v
+        )
+		d = tm.normalize(d)
+		return Ray(self.eye_position, d)
 
-
-		return Ray( self.eye_position, tm.vec3(0,0,-1) )  # temporary ray
